5d6618eeedf01a789e1acfc1eb997136892474fb
donhbryan <36180421+donhbryan@users.noreply.github.com>
2019-04-04 16:14:40 -0500

    add

---
+++ MainWindows.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindows1.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(272, 470)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(30, 100, 201, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.editName = QtWidgets.QLineEdit(self.centralwidget)
+        self.editName.setGeometry(QtCore.QRect(82, 20, 141, 20))
+        self.editName.setObjectName("editName")
+        self.btnOk = QtWidgets.QPushButton(self.centralwidget)
+        self.btnOk.setGeometry(QtCore.QRect(100, 60, 51, 23))
+        self.btnOk.setObjectName("btnOk")
+        self.btnQuit = QtWidgets.QPushButton(self.centralwidget)
+        self.btnQuit.setGeometry(QtCore.QRect(174, 60, 51, 23))
+        self.btnQuit.setObjectName("btnQuit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 20, 47, 13))
+        self.label.setObjectName("label")
+        self.btnBrowse = QtWidgets.QPushButton(self.centralwidget)
+        self.btnBrowse.setGeometry(QtCore.QRect(30, 60, 51, 23))
+        self.btnBrowse.setObjectName("btnBrowse")
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        self.listWidget.setGeometry(QtCore.QRect(10, 160, 241, 261))
+        self.listWidget.setObjectName("listWidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 272, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.btnOk.setText(_translate("MainWindow", "Ok"))
+        self.btnQuit.setText(_translate("MainWindow", "Quit"))
+        self.label.setText(_translate("MainWindow", "Name"))
+        self.btnBrowse.setText(_translate("MainWindow", "Browse"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+mgr_1.print_emps()
+# mgr_1.add_emp(dev_2)
+# mgr_1.print_emps()
