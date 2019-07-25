1f98467f5a4b769a7ba9ac932fa9218d045e1ed7
donhbryan <36180421+donhbryan@users.noreply.github.com>
2019-07-25 13:43:47 -0500

    catch up

---
+++ FileMgmnt/myproj/tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+# tuple
+print("Tuple")
+t = (5, 'program', 1+3j, "French Fries")
+
+# t[1] = 'program'
+print("t[0] = ", t[0])
+print("t[1] = ", t[1])
+print("t[2] = ", t[2])
+
+# t[0:3] = (5, 'program', (1+3j))
+print("t[0:4] = ", t[0:4])
+print("t[2:4] = ", t[2:4])
